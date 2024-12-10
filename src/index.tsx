b2179f692c06a83274f3ee58471e447aec8d1e7d
Je <jemacieldev@gmail.com>
2024-12-10 20:11:36 -0300

    .

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from '../src/App'
-import reportWebVitals from '../src/reportWebVitals'
+import App from './App' // Importa o componente App
+import reportWebVitals from './reportWebVitals' // Importa a função reportWebVitals
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -10,7 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 )
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+
+// Medir o desempenho e logar as métricas no console
+reportWebVitals(console.log)
