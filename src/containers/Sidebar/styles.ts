f14bb7b67631955f6a67add98b0671c5f55389bb
Je <jemacieldev@gmail.com>
2024-12-18 11:36:15 -0300

    ajustes

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -17,4 +17,11 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom:40px;
+    text-align: center;
+
+  }
+
 `;
